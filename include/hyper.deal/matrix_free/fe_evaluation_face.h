80524780a014584bacd09f774c26b602ed2345ce
Peter Munch <peterrmuench@gmail.com>
2020-08-15 22:30:12 +0200

    Access no_faces during ECL+

--- include/hyper.deal/matrix_free/fe_evaluation_face.h
+++ include/hyper.deal/matrix_free/fe_evaluation_face.h
@@ -252,7 +252,9 @@ namespace hyperdeal
               data,
               (is_ecl == false || this->is_minus_face) ?
                 this->face_no :
-                this->face_no ^ 1, // TODO!!!
+                this->matrix_free.get_face_info()
+                  .no_faces[3][(2 * dim * this->macro + this->face_no) *
+                               n_vectors],
               this->macro,
               is_ecl ? 2 : !is_minus_face,
               is_ecl ? 2 * dim * this->macro + this->face_no : this->macro,
